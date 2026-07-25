a970ca7e224f81098bd273f1f478678f08e6ab2a
Karim Q. <kareem.qiblawi@gmail.com>
2026-07-25 10:51:50 +0200

    mosaic

--- Program.cs
+++ Program.cs
@@ -1,35 +1,69 @@
 ﻿using System.Drawing;
 using System.Text.Json;
 using System.IO;
+using ColorMine;
+using ColorMine.ColorSpaces;
+using ColorMine.ColorSpaces.Comparisons;
 
 namespace MosaicImageGeneration
 {
 	public class ThumbnailData
 	{
-		public string? path { get; set; }
-		public Color avgColor { get; set; }
+		public required string path { get; init; }
+		public required Rgb avgColor { get; init; }
 	}
 
 	static class Program
 	{
-		
-		private const int _nChunks = 8; // _nChunks * _nChunks
+		private const int _nChunks = 100; // _nChunks * _nChunks
 		private const string _targetImage = "/Pictures/stp.png";
 		private const string _thumbnailDir = "/Pictures/thumbnails";
 		private const string _thumbnailDataCache = "/Pictures/cache.json";
-		private const string _outputPath = "/Pictures/test/";
+		private const string _outputPath = "/Pictures/results/";
 		
 		private static void Main(string[] args)
 		{
 			var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 			var bmp = new Bitmap(userDir+_targetImage);
+			var chunkWidth = bmp.Width / _nChunks;
+			var chunkHeight = bmp.Height / _nChunks;
 
 			if (!File.Exists(userDir+_thumbnailDataCache))
 				populateThumbnailCache(userDir+_thumbnailDir, userDir+_thumbnailDataCache);
 			
-			var thumbnailImages = JsonSerializer.Deserialize<List<ThumbnailData>>(File.ReadAllText(userDir+_thumbnailDataCache));
+			var thumbnailImages = JsonSerializer.Deserialize<List<ThumbnailData>>(File.ReadAllText(userDir+_thumbnailDataCache))!;
 
-			var chunks = bmp.GenerateChunks();
+			var chunks = bmp.GenerateChunks(chunkWidth, chunkHeight);
+
+			var chunkColorAverages = chunks.Select(c => c.AverageColor()).ToList();
+			
+			var mosaicThumbnails = BestFitThumbnails(chunkColorAverages, thumbnailImages);
+			
+			var finalMosaicImage = StitchMosaic(mosaicThumbnails.Select(mt => new Bitmap(mt)).ToList(), chunkWidth, chunkHeight);
+			
+			finalMosaicImage.Save(userDir+_outputPath+"output.png");
+		}
+		
+		private static Rgb AverageColor(this Bitmap img)
+		{
+			ulong rSum = 0;
+			ulong gSum = 0;
+			ulong bSum = 0;
+			
+			for (var w = 0; w < img.Width; w++)
+				for (var h = 0; h < img.Height; h++)
+				{
+					var pixel = img.GetPixel(w, h);
+					rSum += pixel.R;
+					gSum += pixel.G;
+					bSum += pixel.B;
+				}
+			
+			var avgR = (int) (rSum / (ulong)(img.Width*img.Height));
+			var avgG = (int) (gSum / (ulong)(img.Width*img.Height));
+			var avgB = (int) (bSum / (ulong)(img.Width*img.Height));
+			
+			return new Rgb{R = avgR, G = avgG, B = avgB};
 		}
 
 		private static void populateThumbnailCache(string thumbnailDir, string outputPath)
@@ -41,47 +75,60 @@ namespace MosaicImageGeneration
 			File.WriteAllText(outputPath, JsonSerializer.Serialize(result));
 		}
 
-		private static List<Bitmap> GenerateChunks(this Bitmap img)
+		private static List<Bitmap> GenerateChunks(this Bitmap img, int chunkWidth, int chunkHeight)
 		{
 			var result = new List<Bitmap>();
-			var chunkWidth = img.Width / _nChunks;
-			var chunkHeight = img.Height / _nChunks;
 
 			for (var widthChunkNum = 0; widthChunkNum < _nChunks; widthChunkNum++)
 				for (var heightChunkNum = 0; heightChunkNum < _nChunks; heightChunkNum++)
 				{
 					var chunk = new Bitmap(chunkWidth, chunkHeight);
-					var graphics = Graphics.FromImage(chunk);
+					using var graphics = Graphics.FromImage(chunk);
 					graphics.DrawImage( img, new Rectangle(0, 0, chunkWidth, chunkHeight),
-						new Rectangle(widthChunkNum*chunkWidth, heightChunkNum*chunkHeight, chunkWidth, chunkHeight), GraphicsUnit.Pixel);
-					graphics.Dispose();
+							new Rectangle(widthChunkNum*chunkWidth, heightChunkNum*chunkHeight, chunkWidth, chunkHeight), GraphicsUnit.Pixel);
 					result.Add(chunk);
 				}
 			
 			return result;
 		}
-		
-		private static Color AverageColor(this Bitmap img)
+
+		private static List<string> BestFitThumbnails(List<Rgb> chunkColorsAverages, List<ThumbnailData> thumbnailImages)
 		{
-			ulong rSum = 0;
-			ulong gSum = 0;
-			ulong bSum = 0;
+			var result = new List<string>();
 			
-			for (var w = 0; w < img.Width; w++)
+			foreach (var chunk in chunkColorsAverages)
 			{
-				for (var h = 0; h < img.Height; h++)
+				string? closestImagePath = null;
+				var closestDeltaE = double.MaxValue;
+				foreach (var thumbnailImage in thumbnailImages)
 				{
-					var pixel = img.GetPixel(w, h);
-					rSum += pixel.R;
-					gSum += pixel.G;
-					bSum += pixel.B;
+					var dE = chunk.Compare(thumbnailImage.avgColor, new Cie1976Comparison());
+					if (closestDeltaE < dE)
+						continue;
+					closestDeltaE = dE;
+					closestImagePath = thumbnailImage.path;
 				}
+				result.Add(closestImagePath!);
 			}
-			var avgR = (int) (rSum / (ulong)(img.Width*img.Height));
-			var avgG = (int) (gSum / (ulong)(img.Width*img.Height));
-			var avgB = (int) (bSum / (ulong)(img.Width*img.Height));
-			
-			return Color.FromArgb(avgR, avgG, avgB);
+
+			return result;
+		}
+	
+		private static Bitmap StitchMosaic(List<Bitmap> chunks, int chunkWidth, int chunkHeight)
+		{
+			var result = new Bitmap(chunkWidth * _nChunks, chunkHeight * _nChunks);
+
+			using var graphics = Graphics.FromImage(result);
+			var i = 0;
+			for (var widthChunkNum = 0; widthChunkNum < _nChunks; widthChunkNum++)
+				for (var heightChunkNum = 0; heightChunkNum < _nChunks; heightChunkNum++)
+				{
+					graphics.DrawImage(chunks[i++], 
+					new Rectangle(widthChunkNum * chunkWidth, heightChunkNum * chunkHeight, chunkWidth, chunkHeight),
+					 new Rectangle(0, 0, chunkWidth, chunkHeight), GraphicsUnit.Pixel);
+				}
+
+			return result;
 		}
 	}
 }
